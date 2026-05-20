b3d83b42435e95bc7a88e78b87f914201b3f79b3
Prail <prailexplains@gmail.com>
2026-05-20 12:44:45 +0545

    using loop to clean duplicate data

--- Day 3.py
+++ Day 3.py
@@ -1,3 +1,15 @@
 name = "Prail @@02-- 00"
 new_name = name.replace("@@02","").replace("--","").replace("00","")
 print (new_name)
+
+#In this branch i wil use loops to clean the data, i will remove duplicate names in a list 
+
+names = ["Prail", "Abishek", "Prail", "Suman", "Abishek", "Suman", "Prail"]
+
+clean_names = []
+
+for name in names :
+    if name not in clean_names:
+        clean_names.append(name)
+
+print(clean_names)
