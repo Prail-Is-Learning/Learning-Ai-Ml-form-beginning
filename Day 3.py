05a64c0cb7b2f7e91a3cf1221c855d814953f99f
Prail <prailexplains@gmail.com>
2026-05-21 15:39:44 +0545

    Cleaned Dictonary using loops

--- Day 3.py
+++ Day 3.py
@@ -4,12 +4,40 @@ print (new_name)
 
 #In this branch i wil use loops to clean the data, i will remove duplicate names in a list 
 
-names = ["Prail", "Abishek", "Prail", "Suman", "Abishek", "Suman", "Prail"]
+# names = ["Prail", "Abishek", "Prail", "Suman", "Abishek", "Suman", "Prail"]
 
-clean_names = []
+# clean_names = []
 
-for name in names :
-    if name not in clean_names:
-        clean_names.append(name)
+# for name in names :
+#     if name not in clean_names:
+#         clean_names.append(name)
 
-print(clean_names)
+# print(clean_names)
+
+#Cleaning messy data given by chatGPT by using loops 
+
+usernames = [
+    "  PRAIL_123 ",
+    "alex99",
+    "BOB__GAMER ",
+    "",
+    "john doe",
+    "alex99",
+    "   COOLGUY   "
+]
+
+cleaned_names = []
+
+for names in usernames:
+    names = ( names.strip()
+    .lower()
+    .title()
+    .replace("_","")
+    .replace("__","")
+    .replace("123","")
+    .replace("99","")
+    )
+    if names != "":
+        cleaned_names.append(names)
+
+print(cleaned_names)
