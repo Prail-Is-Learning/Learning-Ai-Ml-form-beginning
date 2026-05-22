c18795d890c19393657ee4e021f961bb194bca4b
Prail <prailexplains@gmail.com>
2026-05-22 12:48:55 +0545

    Using Split

--- Day 3.py
+++ Day 3.py
@@ -1,3 +1,7 @@
-name = "Prail @@02-- 00"
-new_name = name.replace("@@02","").replace("--","").replace("00","")
-print (new_name)
+# name = "Prail @@02-- 00"
+# new_name = name.replace("@@02","").replace("--","").replace("00","")
+# print (new_name)
+raw_data = ("Prail Üno  Coca lala  tasty")
+clean_data = raw_data.split()
+final = " ".join(clean_data)
+print(final)
