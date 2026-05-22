09b34368c0a1f616b280810081ac1df71d41390e
Prail <prailexplains@gmail.com>
2026-05-22 13:35:52 +0545

    Data cleaning

--- Day 3.py
+++ Day 3.py
@@ -1,7 +1,13 @@
 # name = "Prail @@02-- 00"
-# new_name = name.replace("@@02","").replace("--","").replace("00","")
-# print (new_name)
+new_name = name.replace("@@02","").replace("--","").replace("00","")
+print (new_name)
 raw_data = ("Prail Üno  Coca lala  tasty")
 clean_data = raw_data.split()
 final = " ".join(clean_data)
 print(final)
+
+unclean = "Prail-Abishek-Kenji"
+name1 , name2, name3 = unclean.split("-")
+print(name1)
+print(name2)
+print(name3)
