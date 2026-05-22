882bd78150044d1a05d16cddf494b0690aee7486
Prail <prailexplains@gmail.com>
2026-05-22 13:42:23 +0545

    Changes

--- Day 3.py
+++ Day 3.py
@@ -1,4 +1,4 @@
-# name = "Prail @@02-- 00"
+name = "Prail @@02-- 00"
 new_name = name.replace("@@02","").replace("--","").replace("00","")
 print (new_name)
 raw_data = ("Prail Üno  Coca lala  tasty")
